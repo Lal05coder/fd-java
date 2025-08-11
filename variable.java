054f0794cf4d01bf16be6fe0c9a551ce22732de7
Lal05coder <lalitsinghal2k6@gmail.com>
2025-08-11 11:02:25 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main(String args[]) {
+        int a=5;
+        int _a=5;
+        int $a =5;
+        int b=5;
+        int c=5;
+        System.out.println(a);
+        System.out.println(_a);
+        System.out.println($a);
+        System.out.println(b+c);
+    }
+    
+}
