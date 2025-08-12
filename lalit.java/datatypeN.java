297814fbc7462ea78b4915642ab86d45812de491
Lal05coder <lalitsinghal2k6@gmail.com>
2025-08-12 09:44:11 +0530

    Add files via upload

---
+++ lalit.java/datatypeN.java
@@ -0,0 +1,11 @@
+public class datatypeN {
+    public static void main(String args[]) {
+        String name="surya";
+        int ar[]={1,2,3,4,5};
+        int []arr={3,6,7};
+        System.out.println(name);
+        System.out.println(ar[0]);
+        System.out.println(ar[2]);
+        System.out.println(arr[1]);
+    }
+}
